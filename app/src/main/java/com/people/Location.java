afac3386b2ae6ea0a8dfb93eaf755bf2075948a8
dhruvateja <dhruva@svaad.com>
2015-01-27 13:06:29 +0530

    added more ui screens
    me screen, history, user-history, location, create , and more ui
    changes...

--- app/src/main/java/com/people/Location.java
+++ app/src/main/java/com/people/Location.java
@@ -1,42 +1,24 @@
 package com.people;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
+import com.people.R;
 
-
-public class User extends Activity {
-
-    private LinearLayout send_loc_link;
+public class Location extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-
-        send_loc_link = (LinearLayout)findViewById(R.id.mt1);
-        send_loc_link.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(User.this, Location.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_location);
     }
-
-
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user, menu);
+        getMenuInflater().inflate(R.menu.location, menu);
         return true;
     }
 
